72c4c3d444489d36f5072c6a24b1419951285bc0
Lucas-aquiles <lucasechegaray2011@gmail.com>
2024-01-29 00:04:19 -0300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect } from "react";
 import Services from "../components/services"
+import Idea from "../components/idea"
 
 export default function Home() {
   useEffect(() => {
@@ -51,71 +52,7 @@ export default function Home() {
         </div>
       </div>
 {/* --------------------------------------------------------------------------- */}
-      <div className={styles.concepts}>
-        <div data-aos="fade-up"  data-aos-delay="100"
-        data-aos-duration="1000"
-        className={styles.panel}>
-          <Image
-            src="/paper-plane.svg"
-            width={80}
-            height={60}
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <h2 className={styles.title5}>Pensamos el futuro</h2>
-          <div className={styles.hra}></div>
-          <p className={styles.parrafo2}>
-            Proyectamos el futuro en base a nuestros conocimientos científicos,
-            en el marco de la teoría de las Ciencias Sociales, las prácticas
-            cotidianas de las personas y el mundo de las tecnologias .
-          </p>
-        </div>
-
-        <div  className={styles.centrar} data-aos="fade-up"  data-aos-delay="200"
-        data-aos-duration="1000">
-        <div className={styles.panelM}>
-
-          <Image
-            src="/brain.svg"
-            width={80}
-            height={60}
-            alt="Screenshots of the dashboard project showing desktop version"
-            className="iconColor"
-          />
-
-          <h2 className={styles.title5}>Grandes Ideas</h2>
-          <div className={styles.hra}></div>
-
-          <p className={styles.parrafo2}>
-            Aportamos conceptos segun las necesidades de los usuarios y
-            emprendedores. Donde cualquier idea puede ser disruptiva.
-          </p>
-          </div>
-          <div className={styles.myelement}></div>
-
-        </div>
-
-
-        <div className={styles.panel} data-aos="fade-up"  data-aos-delay="300"
-        data-aos-duration="1000">
-          <Image
-            src="/idea.svg"
-            width={80}
-            height={60}
-            alt="Screenshots of the dashboard project showing desktop version"
-            style={{
-              color: "red",
-            }}
-          />
-          <h2 className={styles.title5}>Soluciones Creativas</h2>
-          <div className={styles.hra}></div>
-
-          <p className={styles.parrafo2}>
-            Ya sea que busques transformar industrias enteras o mejorar procesos
-            cotidianos, nosotros no solo proporcionamos soluciones; creamos
-            experiencias revolucionarias.
-          </p>
-        </div>
-      </div>
+            <Idea/>
       {/* --------------------------------------------------- */}
             <Services/>
     </main>
---
+++ src/components/idea.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import styles from "./idea.module.css";
+
+
+export default function Idea(){
+
+    return( 
+        <div className={styles.father}>
+    <div className={styles.concepts}>
+        {/*  -----------------     */}
+    <div data-aos="fade-up"  data-aos-delay="100"
+    data-aos-duration="1000"
+    className={styles.panel}>
+      <Image
+        src="/paper-plane.svg"
+        width={80}
+        height={60}
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      <h2 className={styles.title5}>Pensamos el futuro</h2>
+      <div className={styles.hra}></div>
+      <p className={styles.parrafo2}>
+        Proyectamos el futuro en base a nuestros conocimientos científicos,
+        en el marco de la teoría de las Ciencias Sociales, las prácticas
+        cotidianas de las personas y el mundo de las tecnologias .
+      </p>
+    </div>
+        {/*  -----------------     */}
+
+    <div  className={styles.panelM} data-aos="fade-up"  data-aos-delay="200"
+    data-aos-duration="1000">
+
+      <Image
+        src="/brain.svg"
+        width={80}
+        height={60}
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+
+      <h2 className={styles.title5}>Grandes Ideas</h2>
+      <div className={styles.hra}></div>
+
+      <p className={styles.parrafo2}>
+        Aportamos conceptos segun las necesidades de los usuarios y
+        emprendedores. Donde cualquier idea puede ser disruptiva.
+      </p>
+
+    </div>
+
+        {/*  -----------------     */}
+
+    <div className={styles.panel} data-aos="fade-up"  data-aos-delay="300"
+    data-aos-duration="1000">
+      <Image
+        src="/idea.svg"
+        width={80}
+        height={60}
+        alt="Screenshots of the dashboard project showing desktop version"
+       
+      />
+      <h2 className={styles.title5}>Soluciones Creativas</h2>
+      <div className={styles.hra}></div>
+
+      <p className={styles.parrafo2}>
+        Ya sea que busques transformar industrias enteras o mejorar procesos
+        cotidianos, nosotros no solo proporcionamos soluciones; creamos
+        experiencias revolucionarias.
+      </p>
+    </div>
+  </div>
+      <div className={styles.myelementFather}>
+        <div className={styles.myelement}> </div>
+      </div>
+
+  </div>
+
+    )
+}
