e25add4154bc0fa8fd59437bae0dc41ef22e6e5e
Lucas-aquiles <lucasechegaray2011@gmail.com>
2024-03-29 21:30:55 -0300

    changes in the styles

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./services.module.css";
+import Image from "next/image";
+
 export default function Services() {
   return (
     <div className={styles.generalDiv}>
@@ -11,32 +13,88 @@ export default function Services() {
       <h3 className={styles.title1}>NUESTROS SERVICIOS PARA LOS CLIENTES</h3>
 
       <div className={styles.fathercontainer} >
-
+      <div className={styles.container}>
+      <Image
+            src="/titulo1.svg"
+            width={70}
+            height={70}
+            
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <h3>Soluciones Digitales </h3>
+          <p>Desarrollo de Aplicaciones Web, Software Internos y Aplicaciones Móviles</p>
+        </div>
         <div className={styles.container}>
-          <p>Estrategia Digital</p>
+        <Image
+            src="/titulo2.svg"
+            width={70}
+            height={70}
+            
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <h3>Estrategia Digital</h3>
+          <p>Aprovecha al máximo las oportunidades que ofrece el entorno digital para alcanzar tus objetivos comerciales</p>
         </div>
 
-        <div className={styles.container}>
-          <p>Diseños UX/IX</p>
+        <div className={styles.container1}>
+        <Image
+            src="/titulo3.svg"
+            width={70}
+            height={70}
+            
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <h3>Diseños UX/IX</h3>
+          <p>Pensamos y diseñamos , productos digitales que sean intuitivos, atractivos y satisfactorios para los usuarios</p>
         </div>
 
-        <div className={styles.container}>
-          <p>Redes Sociales</p>
-        </div>
+       
       </div>
 
 
       <div className={styles.fathercontainer}>
         <div className={styles.container}>
-          <p>Diseño Conceptual</p>
+        <Image
+            src="/titulo4.svg"
+            width={70}
+            height={70}
+            
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <h3>Consultoria</h3>
+          <p> Análisis de Mercado y Opinión Pública.
+          Mapeamos el clima social y la penetración y aceptación de marcas y productos mediante investigación cualitativa y cuantitativa.
+          </p>
         </div>
 
         <div className={styles.container}>
-          <p>Consultoria</p>
+        <Image
+            src="/titulo5.svg"
+            width={70}
+            height={70}
+            
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <h3>Automatización de Procesos </h3>
+          <p>Desarrollar sistemas y flujos de trabajo automatizados para agilizar el procesamiento de datos y reducir
+             la intervención humana en tareas repetitivas o rutinarias</p>
         </div>
 
         <div className={styles.container}>
-          <p>Estadisticas</p>
+        <Image
+            src="/titulo6.svg"
+            width={70}
+            height={70}
+            
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <h3> Investigación Cualitativa: </h3>
+          <p> Grupos Focales</p>
+          <p> Entrevistas en Profundidad </p>
+          <h3> Investigación Cuantitativa: </h3>
+          <p> Entrevistas Domiciliarias</p>
+          <p> Entrevistas Telefónicas</p>
+          <p> Entrevistas en Puntos de Encuentro</p>
         </div>
       </div>
     </div>
