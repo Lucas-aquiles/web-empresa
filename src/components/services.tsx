349901618e4ad7d201fc5972186aab3ca2a0a44b
manu-lucas <lmechegaray@wecom.global>
2024-04-06 16:39:19 -0300

    add changes in services

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -5,87 +5,102 @@ import Image from "next/image";
 export default function Services() {
   return (
     <div className={styles.generalDiv}>
-      
-      <div className={styles.centrar}> 
-      <div className={styles.lyne}></div>
-      <h3 className={styles.title}>Servicios.</h3>
+      <div className={styles.centrar}>
+        <div className={styles.lyne}></div>
+        <h3 className={styles.title}>Servicios.</h3>
       </div>
       <h3 className={styles.title1}>NUESTROS SERVICIOS PARA LOS CLIENTES</h3>
 
-      <div className={styles.fathercontainer} >
-      <div className={styles.container}>
-      <Image
+      <div className={styles.fathercontainer}>
+        <div className={styles.container}>
+          <Image
             src="/titulo1.svg"
             width={70}
             height={70}
-            
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <h3>Soluciones Digitales </h3>
-          <p>Desarrollo de Aplicaciones Web, Software Internos y Aplicaciones Móviles</p>
+          <p>
+            Desarrollo de Aplicaciones Web, Software Internos y Aplicaciones
+            Móviles
+          </p>
+
+          <div className={styles.lineaDiv}></div>
         </div>
         <div className={styles.container}>
-        <Image
+          <Image
             src="/titulo2.svg"
             width={70}
             height={70}
-            
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <h3>Estrategia Digital</h3>
-          <p>Aprovecha al máximo las oportunidades que ofrece el entorno digital para alcanzar tus objetivos comerciales</p>
+          <p>
+            Aprovecha al máximo las oportunidades que ofrece el entorno digital
+            para alcanzar tus objetivos comerciales
+          </p>
+          <div className={styles.lineaDiv}></div>
+
         </div>
 
         <div className={styles.container1}>
-        <Image
+          <Image
             src="/titulo3.svg"
             width={70}
             height={70}
-            
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <h3>Diseños UX/IX</h3>
-          <p>Pensamos y diseñamos , productos digitales que sean intuitivos, atractivos y satisfactorios para los usuarios</p>
+          <p>
+            Pensamos y diseñamos , productos digitales que sean intuitivos,
+            atractivos y satisfactorios para los usuarios
+          </p>
+          <div className={styles.lineaDiv}></div>
+
         </div>
-
-       
       </div>
-
 
       <div className={styles.fathercontainer}>
         <div className={styles.container}>
-        <Image
+          <Image
             src="/titulo4.svg"
             width={70}
             height={70}
-            
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <h3>Consultoria</h3>
-          <p> Análisis de Mercado y Opinión Pública.
-          Mapeamos el clima social y la penetración y aceptación de marcas y productos mediante investigación cualitativa y cuantitativa.
+          <p>
+            {" "}
+            Análisis de Mercado y Opinión Pública. Mapeamos el clima social y la
+            penetración y aceptación de marcas y productos mediante
+            investigación cualitativa y cuantitativa.
           </p>
+          <div className={styles.lineaDiv}></div>
+
         </div>
 
         <div className={styles.container}>
-        <Image
+          <Image
             src="/titulo5.svg"
             width={70}
             height={70}
-            
             alt="Screenshots of the dashboard project showing desktop version"
           />
-          <h3>Automatización de Procesos </h3>
-          <p>Desarrollar sistemas y flujos de trabajo automatizados para agilizar el procesamiento de datos y reducir
-             la intervención humana en tareas repetitivas o rutinarias</p>
+          <h3>Automatización </h3>
+          <p>
+            Desarrollar sistemas y flujos de trabajo automatizados para agilizar
+            el procesamiento de datos y reducir la intervención humana en tareas
+            repetitivas o rutinarias
+          </p>
+          <div className={styles.lineaDiv}></div>
+
         </div>
 
         <div className={styles.container}>
-        <Image
+          <Image
             src="/titulo6.svg"
             width={70}
             height={70}
-            
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <h3> Investigación Cualitativa: </h3>
@@ -94,7 +109,8 @@ export default function Services() {
           <h3> Investigación Cuantitativa: </h3>
           <p> Entrevistas Domiciliarias</p>
           <p> Entrevistas Telefónicas</p>
-          <p> Entrevistas en Puntos de Encuentro</p>
+          <div className={styles.lineaDiv}></div>
+
         </div>
       </div>
     </div>
